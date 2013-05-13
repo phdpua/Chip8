631e92751237a2fabecee8d1c83223f5721861d3
phdpua <phdpua@gmail.com>
2013-05-14 00:47:26 +0300

    Added SoundCard class

---
+++ Chip8Emulator/Chip8Emulator/Sound/SoundCard.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Chip8Emulator
+{
+    public class SoundCard
+    {
+        public void Beep()
+        {
+            //Generate sound stream
+
+            //Play sound stream
+            SoundEffect player = SoundEffect.FromStream(null);
+        }
+    }
+}
